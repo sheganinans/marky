8453bf9730c6198f04ee9df3a1211a751081ce40
Aistis Raulinaitis <a.raulinaitis@protonmail.ch>
2020-11-22 20:32:31 -0800

    reorder

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,74 @@ impl<'de> Deserialize<'de> for F {
     }
 }
 
+fn main() {
+    let matches = clap_app!(marky =>
+        (version: "0.0.6")
+        (author: "Aistis Raulinaitis. <sheganians@gmail.com>")
+        (about: "marky, the CSV time series MCMC trainer")
+        (@arg DESIRED_LEN: +required "desired length of history")
+        (@arg INPUT: +required "input file")
+        (@arg OUTPUT: -o --output +takes_value "output destination")
+        (@arg CHUNKING: -c --chunking +takes_value "chunking factor (default 10)")
+        (@arg CHUNK_DELTA: -t --delta +takes_value "chunking delta (default φ)")
+        (@arg NUM_FILES: -n --num +takes_value "generate n mumber of files named `n.out.csv`")
+        (@arg DIVISOR: -r --divisor +takes_value "set `max(len(chunks)) < len(rows)/divisor` (useful for large files)")
+        (@arg SILENT: -s --silent "make me shut up")
+        (@arg HEADER: --header "has header (default false)")
+        (@arg ORDER: -d ... "increase order of MCMC")
+        (@arg HL2: --hl2 "HL2 mode")
+        (@arg OHLC: --ohlc "OHLC mode")
+        (@arg OHLCV: --ohlcv "OHLCV mode")
+        (@arg F64: --f64 "f64 mode (default true)")
+        (@arg I64: --i64 "i64 mode")
+        (@arg U64: --u64 "u64 mode")
+    ).get_matches();
+
+    let input = matches.value_of("INPUT").unwrap();
+    let desired_len = matches.value_of("DESIRED_LEN").unwrap().parse().unwrap();
+    let output = matches.value_of("OUTPUT").unwrap_or("out.csv");
+    let chunking = matches.value_of("CHUNKING").unwrap_or("10").parse().unwrap();
+    let chunk_delta : f64 = matches.value_of("CHUNK_DELTA").unwrap_or("1.618033988749894848204586834").parse().unwrap();
+    let num_files = matches.value_of("NUM_FILES").unwrap_or("1").parse().unwrap();
+    let divisor = matches.value_of("DIVISOR").unwrap_or("1").parse().unwrap();
+    let silent = matches.is_present("SILENT");
+    let header = matches.is_present("HEADER");
+    let order = matches.occurrences_of("ORDER") as usize;
+    let hl2_mode = matches.is_present("HL2");
+    let ohlc_mode = matches.is_present("OHLC");
+    let ohlcv_mode = matches.is_present("OHLCV");
+    let f64_mode = matches.is_present("F64");
+    let i64_mode = matches.is_present("I64");
+    let u64_mode = matches.is_present("U64");
+
+    enum Mode { HL2, OHLC, OHLCV, F64, I64, U64 }
+
+    let go = |mode: Mode| {
+        (match mode {
+            Mode::HL2   => gen::<HL2>,
+            Mode::OHLC  => gen::<OHLC>,
+            Mode::OHLCV => gen::<OHLCV>,
+            Mode::F64   => gen::<Vec<F>>,
+            Mode::I64   => gen::<Vec<i64>>,
+            Mode::U64   => gen::<Vec<u64>>,
+        })(input, desired_len, output, chunking, chunk_delta, num_files, divisor, silent, header, order)
+    };
+    let ret = match (hl2_mode, ohlc_mode, ohlcv_mode, f64_mode, i64_mode, u64_mode) {
+        (false, false, false, false, false, false) => go(Mode::F64),
+        ( true, false, false, false, false, false) => go(Mode::HL2),
+        (false,  true, false, false, false, false) => go(Mode::OHLC),
+        (false, false,  true, false, false, false) => go(Mode::OHLCV),
+        (false, false, false,  true, false, false) => go(Mode::F64),
+        (false, false, false, false,  true, false) => go(Mode::I64),
+        (false, false, false, false, false,  true) => go(Mode::U64),
+        _ => Err(String::from("more than one modes selected!").into())
+    };
+    match ret {
+        Ok(_) => if !silent { println!("done!") },
+        Err(e) => println!("{}", e)
+    }
+}
+
 fn gen<Row : Eq + Hash + Clone + Sync + Serialize + DeserializeOwned>
     ( input: &str
     , desired_len: usize
@@ -119,72 +187,4 @@ fn gen<Row : Eq + Hash + Clone + Sync + Serialize + DeserializeOwned>
     let duration = start.elapsed();
     if !silent { println!("time elapsed writing files: {:?}", duration); }
     Ok(())
-}
-
-fn main() {
-    let matches = clap_app!(marky =>
-        (version: "0.0.6")
-        (author: "Aistis Raulinaitis. <sheganians@gmail.com>")
-        (about: "marky, the CSV time series MCMC trainer")
-        (@arg DESIRED_LEN: +required "desired length of history")
-        (@arg INPUT: +required "input file")
-        (@arg OUTPUT: -o --output +takes_value "output destination")
-        (@arg CHUNKING: -c --chunking +takes_value "chunking factor (default 10)")
-        (@arg CHUNK_DELTA: -t --delta +takes_value "chunking delta (default φ)")
-        (@arg NUM_FILES: -n --num +takes_value "generate n mumber of files named `n.out.csv`")
-        (@arg DIVISOR: -r --divisor +takes_value "set `max(len(chunks)) < len(rows)/divisor` (useful for large files)")
-        (@arg SILENT: -s --silent "make me shut up")
-        (@arg HEADER: --header "has header (default false)")
-        (@arg ORDER: -d ... "increase order of MCMC")
-        (@arg HL2: --hl2 "HL2 mode")
-        (@arg OHLC: --ohlc "OHLC mode")
-        (@arg OHLCV: --ohlcv "OHLCV mode")
-        (@arg F64: --f64 "f64 mode (default true)")
-        (@arg I64: --i64 "i64 mode")
-        (@arg U64: --u64 "u64 mode")
-    ).get_matches();
-
-    let input = matches.value_of("INPUT").unwrap();
-    let desired_len = matches.value_of("DESIRED_LEN").unwrap().parse().unwrap();
-    let output = matches.value_of("OUTPUT").unwrap_or("out.csv");
-    let chunking = matches.value_of("CHUNKING").unwrap_or("10").parse().unwrap();
-    let chunk_delta : f64 = matches.value_of("CHUNK_DELTA").unwrap_or("1.618033988749894848204586834").parse().unwrap();
-    let num_files = matches.value_of("NUM_FILES").unwrap_or("1").parse().unwrap();
-    let divisor = matches.value_of("DIVISOR").unwrap_or("1").parse().unwrap();
-    let silent = matches.is_present("SILENT");
-    let header = matches.is_present("HEADER");
-    let order = matches.occurrences_of("ORDER") as usize;
-    let hl2_mode = matches.is_present("HL2");
-    let ohlc_mode = matches.is_present("OHLC");
-    let ohlcv_mode = matches.is_present("OHLCV");
-    let f64_mode = matches.is_present("F64");
-    let i64_mode = matches.is_present("I64");
-    let u64_mode = matches.is_present("U64");
-
-    enum Mode { HL2, OHLC, OHLCV, F64, I64, U64 }
-
-    let go = |mode: Mode| {
-        (match mode {
-            Mode::HL2   => gen::<HL2>,
-            Mode::OHLC  => gen::<OHLC>,
-            Mode::OHLCV => gen::<OHLCV>,
-            Mode::F64   => gen::<Vec<F>>,
-            Mode::I64   => gen::<Vec<i64>>,
-            Mode::U64   => gen::<Vec<u64>>,
-        })(input, desired_len, output, chunking, chunk_delta, num_files, divisor, silent, header, order)
-    };
-    let ret = match (hl2_mode, ohlc_mode, ohlcv_mode, f64_mode, i64_mode, u64_mode) {
-        (false, false, false, false, false, false) => go(Mode::F64),
-        ( true, false, false, false, false, false) => go(Mode::HL2),
-        (false,  true, false, false, false, false) => go(Mode::OHLC),
-        (false, false,  true, false, false, false) => go(Mode::OHLCV),
-        (false, false, false,  true, false, false) => go(Mode::F64),
-        (false, false, false, false,  true, false) => go(Mode::I64),
-        (false, false, false, false, false,  true) => go(Mode::U64),
-        _ => Err(String::from("more than one modes selected!").into())
-    };
-    match ret {
-        Ok(_) => if !silent { println!("done!") },
-        Err(e) => println!("{}", e)
-    }
 }
